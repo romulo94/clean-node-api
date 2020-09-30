a5b0d0972eda67508d6d1d2fd034993dc0284d6a
Rômulo Rocha <romulorocha063@gmail.com>
2020-09-30 15:25:48 -0300

    refactor: customized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,35 +1,36 @@
-import { SignUpController } from './signup';
+import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-params-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
         password: 'any_password',
         passwordConfirmation: 'any_passwordConfirmation',
       },
-    };
+    }
 
-    const httpResponse = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest)
 
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
-  });
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         name: 'any_name',
         password: 'any_password',
         passwordConfirmation: 'any_passwordConfirmation',
       },
-    };
+    }
 
-    const httpResponse = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest)
 
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
-  });
-});
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+})
