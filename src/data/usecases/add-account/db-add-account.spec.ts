8fdffdb487aff8d2d7458de1ac0b3b94a9653af7
Rômulo Rocha <romulorocha063@gmail.com>
2020-10-03 20:58:38 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -124,4 +124,23 @@ describe('DbAccount Usecase', () => {
 
     await expect(promisse).rejects.toThrow()
   })
+
+  test('Should returns an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    })
+  })
 })
