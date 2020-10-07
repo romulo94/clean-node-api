29588c0dcd925c4b097ccaeebc41e05ccdd49cd4
Rômulo Rocha <romulorocha063@gmail.com>
2020-10-06 22:48:10 -0300

    feat: ensure signup routes return an account onn success

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Cors Middleware', () => {
+describe('Content Type Middleware', () => {
   test('Should return default content type as json', async () => {
     app.get('/test_content_type', (req, res) => {
       res.send()
