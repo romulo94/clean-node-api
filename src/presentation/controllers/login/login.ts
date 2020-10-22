8c2750529cd3a0d21fb023526bf39211c6b15c7c
Rômulo Rocha <romulorocha063@gmail.com>
2020-10-22 16:02:23 -0300

    feat: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,9 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import {
+  badRequest,
+  serverError,
+  unauthorized,
+} from '../../helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
 import { Authentication } from '../../../domain/usecases/authentication'
@@ -16,22 +20,17 @@ export class LoginController implements Controller {
     try {
       const { email, password } = httpRequest.body
       if (!email) {
-        return new Promise((resolve) =>
-          resolve(badRequest(new MissingParamError('email')))
-        )
+        return badRequest(new MissingParamError('email'))
       }
       if (!password) {
-        return new Promise((resolve) =>
-          resolve(badRequest(new MissingParamError('password')))
-        )
+        return badRequest(new MissingParamError('password'))
       }
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return new Promise((resolve) =>
-          resolve(badRequest(new InvalidParamError('email')))
-        )
+        return badRequest(new InvalidParamError('email'))
       }
-      await this.authentication.auth(email, password)
+      const accessToken = await this.authentication.auth(email, password)
+      if (!accessToken) return unauthorized()
     } catch (error) {
       return serverError(error)
     }
