857eea1549d94818e6a64af06fe16edafda62df0
Rômulo Rocha <romulorocha063@gmail.com>
2020-10-22 17:41:10 -0300

    refactor: remove unsed test

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -6,7 +6,7 @@ import {
   AddAccount,
   Validation,
 } from './signup-protocols'
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
@@ -28,17 +28,6 @@ export class SignUpController implements Controller {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) return badRequest(error)
-      const requiredFields = [
-        'name',
-        'email',
-        'password',
-        'passwordConfirmation',
-      ]
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field])
-          return badRequest(new MissingParamError(field))
-      }
 
       const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation)
