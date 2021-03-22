6b975870f289300443c5de2fb03dc6d3a9590b22
Romulo Rocha <romulorocha063@gmail.com>
2021-03-22 07:46:50 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,7 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
-export { Hasher, AddAccountRepository } from '../../protocols'
+export {
+  Hasher,
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+} from '../../protocols'
